5b8e1fa390c2597ee8dc42dd3e0e2ab7f7e05b06
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-05-16 02:36:36 +0300

    import used libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <h1 className="text-danger">Hello</h1>
         </>
     );
 }
