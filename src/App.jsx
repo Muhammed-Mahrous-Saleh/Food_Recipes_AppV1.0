55d00421bf3f8c1c9c105d266972f54a93aead86
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-05-16 03:05:19 +0300

    add svgr vite plugin to deal with custom icons svg files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import PhoneIcon from "./assets/icons/phone.svg?react";
 import "./App.css";
 
 function App() {
     return (
         <>
-            <h1 className="text-danger">Hello</h1>
+            <h1 className="text-success">
+                <i className="fa-regular fa-face-smile"></i>
+                <PhoneIcon className="text-danger" />
+                Hello World
+            </h1>
         </>
     );
 }
