3c85111c6c2f855c412a8767a11810fb3f1ab75d
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-05-19 18:59:03 +0300

    update logo links

--- src/modules/Shared/components/authlayout/AuthLayout.jsx
+++ src/modules/Shared/components/authlayout/AuthLayout.jsx
@@ -10,7 +10,7 @@ const AuthLayout = () => {
                         <div className="auth-card col-md-5 bg-white rounded-3">
                             <div className="logo-container text-center">
                                 <img
-                                    src="/src/assets/images/logoBrand.png"
+                                    src="src/assets/images/logoBrand.png"
                                     alt="logo brand with title"
                                     className="w-75"
                                 />
