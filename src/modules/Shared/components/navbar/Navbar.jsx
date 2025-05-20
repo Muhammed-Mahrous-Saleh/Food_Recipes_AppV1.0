05d7416cb0d727db4a652c531a6d51cc740003b8
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-05-20 13:07:42 +0300

    add nav profile image placeholder

--- src/modules/Shared/components/navbar/Navbar.jsx
+++ src/modules/Shared/components/navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import logo from "@/assets/images/logo.png";
+import profile from "@/assets/images/profile.png";
 import Alert from "@/assets/icons/alert.svg?react";
 
 const Navbar = ({ loginData }) => {
     return (
         <div className="navbar-container p-3">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light p-3 rounded-3">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light p-3 rounded-4">
                 <div className="container-fluid justify-content-end">
                     <div className="user-info-container d-flex justify-content-center align-items-center ">
                         <div className="user-img-container">
-                            <img src={logo} alt="user image" />
+                            <img src={profile} alt="user image" />
                         </div>
                         <div className="user-name-container">
                             {loginData?.userName}
@@ -57,17 +57,17 @@ const Navbar = ({ loginData }) => {
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li>
                                 <a class="dropdown-item" href="#">
-                                    Action
+                                    Alert
                                 </a>
                             </li>
                             <li>
                                 <a class="dropdown-item" href="#">
-                                    Another action
+                                    Another Alert
                                 </a>
                             </li>
                             <li>
                                 <a class="dropdown-item" href="#">
-                                    Something else here
+                                    Alert
                                 </a>
                             </li>
                         </ul>
