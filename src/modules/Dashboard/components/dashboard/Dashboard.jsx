3897cee83984e327922d630f29309b1eebb95af9
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-05-20 14:51:19 +0300

    add dashboard page

--- src/modules/Dashboard/components/dashboard/Dashboard.jsx
+++ src/modules/Dashboard/components/dashboard/Dashboard.jsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../../../Shared/components/header/Header";
+import dashboardImg from "@/assets/images/dashboard-header-img.png";
 
 const Dashboard = () => {
     return (
         <>
             <Header
                 title={"Welcome"}
-                subTitle={"Upskilling!"}
+                subTitle={"Upskilling !"}
                 description={
                     "This is a welcoming screen for the entry of the application , you can now see the options"
                 }
+                imgsrc={dashboardImg}
             />
+            <div className="dashboard-content px-3">
+                <div className="fill-recipes-card p-4 d-flex justify-content-between align-items-center rounded-2">
+                    <div className="card-text col-5">
+                        <h3>
+                            Fill the <span>Recipes</span> !
+                        </h3>
+                        <p>
+                            you can now fill the meals easily using the table
+                            and form , click here and sill it with the table !
+                        </p>
+                    </div>
+                    <Link to="recipes" className="card-action-btn btn col-2">
+                        Fill Recipes<i class="fa-solid fa-arrow-right"></i>
+                    </Link>
+                </div>
+            </div>
         </>
     );
 };
