1e49bfae8d39be3e58ae482a4e1e311ccf341451
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-05-20 12:04:48 +0300

    add navbar

--- src/modules/Shared/components/navbar/Navbar.jsx
+++ src/modules/Shared/components/navbar/Navbar.jsx
@@ -1,38 +1,80 @@
 import React from "react";
+import logo from "@/assets/images/logo.png";
+import Alert from "@/assets/icons/alert.svg?react";
 
 const Navbar = ({ loginData }) => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarSupportedContent"
-                >
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a
-                                className="nav-link active"
-                                aria-current="page"
-                                href="#"
-                            >
-                                {loginData?.userName}
-                            </a>
-                        </li>
-                    </ul>
+        <div className="navbar-container p-3">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light p-3 rounded-3">
+                <div className="container-fluid justify-content-end">
+                    <div className="user-info-container d-flex justify-content-center align-items-center ">
+                        <div className="user-img-container">
+                            <img src={logo} alt="user image" />
+                        </div>
+                        <div className="user-name-container">
+                            {loginData?.userName}
+                        </div>
+                    </div>
+                    <div class="dropdown mx-4">
+                        <a
+                            href="#"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            <i class="fa-solid fa-chevron-down"></i>
+                        </a>
+
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    Action
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    Another action
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    Something else here
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="dropdown mx-4">
+                        <a
+                            className="alert-container"
+                            href="#"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            <Alert />
+                        </a>
+
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    Action
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    Another action
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    Something else here
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 };
 
